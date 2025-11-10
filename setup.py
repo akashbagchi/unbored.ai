40de324f267c6295d76c2cbd52ff90bb3ee90935
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2025-11-10 12:54:02 -0700

    chore: update requirements and entry point

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'onboard=unbored.cli:main',
+            'unbored me=unbored.cli:main',
         ],
     },
     include_package_data=True,
